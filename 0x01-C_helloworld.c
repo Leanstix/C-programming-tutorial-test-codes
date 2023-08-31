f254098e957b78f11c7b089de50e4c6620d0bbe1
Leanstix <leanstixx@gmail.com>
2023-09-01 00:58:36 +0100

    the first tutorial commit

---
+++ 0x01-C_helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - This code prints "hello world" usimg the printf fumction.
+ * #include <stdio.h> this is us including the standard input and output of the standard c library.
+ * Return - wherever you see this then just know that it if the value is 0 the code runs without error but if the value is 1 the code has and error.
+ */
+int main(void)
+{
+	printf("Hello world!\n");
+	return 0;
+}
